c6536e0eb4f0a571f7568613a21400c8f3107336
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-11-16 13:41:06 +0100

    Formation Symfony : 03- Monblog_nouvelletechno : initialisation

---
+++ 03- Monblog_nouvelletechno/migrations/Version20220527164033.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220527164033 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE articles CHANGE slug slug VARCHAR(128) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_BFDD3168989D9B62 ON articles (slug)');
+        $this->addSql('ALTER TABLE categories CHANGE slug slug VARCHAR(128) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_3AF34668989D9B62 ON categories (slug)');
+        $this->addSql('ALTER TABLE mots_cles CHANGE slug slug VARCHAR(128) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_D4E4C6CA989D9B62 ON mots_cles (slug)');
+        $this->addSql('ALTER TABLE users ADD activation_token VARCHAR(50) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_BFDD3168989D9B62 ON articles');
+        $this->addSql('ALTER TABLE articles CHANGE slug slug VARCHAR(255) NOT NULL');
+        $this->addSql('DROP INDEX UNIQ_3AF34668989D9B62 ON categories');
+        $this->addSql('ALTER TABLE categories CHANGE slug slug VARCHAR(60) NOT NULL');
+        $this->addSql('DROP INDEX UNIQ_D4E4C6CA989D9B62 ON mots_cles');
+        $this->addSql('ALTER TABLE mots_cles CHANGE slug slug VARCHAR(60) NOT NULL');
+        $this->addSql('ALTER TABLE users DROP activation_token');
+    }
+}
