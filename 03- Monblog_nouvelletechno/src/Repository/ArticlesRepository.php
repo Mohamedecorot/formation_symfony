d3286a72986f27688d03e1b1243b53434ae560ff
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-11-25 13:18:59 +0100

    Formation Symfony : 03- Monblog_nouvelletechno : 9- Créer une API

--- 03- Monblog_nouvelletechno/src/Repository/ArticlesRepository.php
+++ 03- Monblog_nouvelletechno/src/Repository/ArticlesRepository.php
@@ -24,6 +24,20 @@ class ArticlesRepository extends ServiceEntityRepository
     }
 
     /**
+     * @return Articles[] Returns an array of Articles objects
+    */
+    public function apiFindAll() : array
+    {
+        $qb = $this->createQueryBuilder('a')
+            ->select('a.id', 'a.titre', 'a.contenu', 'a.featured_image', 'a.created_at')
+            ->orderBy('a.created_at', 'DESC');
+
+        $query = $qb->getQuery();
+
+        return $query->execute();
+    }
+
+    /**
      * @throws ORMException
      * @throws OptimisticLockException
      */
