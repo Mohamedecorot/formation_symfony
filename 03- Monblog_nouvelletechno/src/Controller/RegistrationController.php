709f9f3d0e2e5a08e76e24771295f88385aebd7d
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-11-30 12:49:21 +0100

    Formation Symfony : 03- Monblog_nouvelletechno : 13- Activation de compte et récupération de mot de passe

--- 03- Monblog_nouvelletechno/src/Controller/RegistrationController.php
+++ 03- Monblog_nouvelletechno/src/Controller/RegistrationController.php
@@ -4,15 +4,16 @@ namespace App\Controller;
 
 use App\Entity\Users;
 use App\Form\RegistrationFormType;
+use App\Repository\UsersRepository;
 use App\Security\UsersAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistrationController extends AbstractController
 {
@@ -34,9 +35,28 @@ class RegistrationController extends AbstractController
                 )
             );
 
+            // On génère un token et on l'enregistre
+            $user->setActivationToken(md5(uniqid()));
+
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
+            // On crée le message
+            $message = (new \Swift_Message('Nouveau compte'))
+            // On attribue l'expéditeur
+            ->setFrom('votre@adresse.fr')
+            // On attribue le destinataire
+            ->setTo($user->getEmail())
+            // On crée le texte avec la vue
+            ->setBody(
+                $this->renderView(
+                    'emails/activation.html.twig', ['token' => $user->getActivationToken()]
+                ),
+                'text/html'
+            )
+            ;
+            $mailer->send($message);
+
 
             return $guardHandler->authenticateUserAndHandleSuccess(
                 $user,
@@ -49,5 +69,32 @@ class RegistrationController extends AbstractController
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/activation/{token}", name="activation")
+     */
+    public function activation($token, UsersRepository $users)
+    {
+        // On recherche si un utilisateur avec ce token existe dans la base de données
+        $user = $users->findOneBy(['activation_token' => $token]);
+
+        // Si aucun utilisateur n'est associé à ce token
+        if(!$user){
+            // On renvoie une erreur 404
+            throw $this->createNotFoundException('Cet utilisateur n\'existe pas');
+        }
+
+        // On supprime le token
+        $user->setActivationToken(null);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        // On génère un message
+        $this->addFlash('message', 'Utilisateur activé avec succès');
+
+        // On retourne à l'accueil
+        return $this->redirectToRoute('accueil');
     }
 }
--- 03- Monblog_nouvelletechno/src/Entity/Users.php
+++ 03- Monblog_nouvelletechno/src/Entity/Users.php
@@ -43,6 +43,15 @@ class Users implements UserInterface
      */
     private $articles;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $activation_token;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $reset_token;
 
     public function __construct()
     {
@@ -161,6 +170,30 @@ class Users implements UserInterface
                 $article->setUsers(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getActivationToken(): ?string
+    {
+        return $this->activation_token;
+    }
+
+    public function setActivationToken(?string $activation_token): self
+    {
+        $this->activation_token = $activation_token;
+
+        return $this;
+    }
+
+    public function getResetToken(): ?string
+    {
+        return $this->reset_token;
+    }
+
+    public function setResetToken(?string $reset_token): self
+    {
+        $this->reset_token = $reset_token;
 
         return $this;
     }
