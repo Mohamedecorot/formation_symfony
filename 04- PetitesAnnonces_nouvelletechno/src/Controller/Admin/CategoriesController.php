438e7ce7da7b18fecbfdfa5e31c9fc22636f5b75
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-12-08 09:37:27 +0100

    Formation Symfony : 04- PetitesAnnonces_nouvelletechno: 06- Créer une interface d'administration

---
+++ 04- PetitesAnnonces_nouvelletechno/src/Controller/Admin/CategoriesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Categories;
+use App\Form\CategoriesType;
+use App\Repository\CategoriesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/categories", name="admin_categories_")
+ * @package App\Controller\Admin
+ */
+class CategoriesController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(CategoriesRepository $categoriesRepository): Response
+    {
+        return $this->render('admin/categories/index.html.twig', [
+            'categories' => $categoriesRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/ajout', name: 'ajout')]
+    public function ajoutCategorie(Request $request): Response
+    {
+        $categorie = new Categories;
+
+        $form = $this->createForm(CategoriesType::class, $categorie);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($categorie);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_categories_home');        }
+
+        return $this->render('admin/categories/ajout.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/modifier/{id}', name: 'modifier')]
+    public function ModifCategorie(Categories $categorie, Request $request)
+    {
+        $form = $this->createForm(CategoriesType::class, $categorie);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($categorie);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_categories_home');
+        }
+
+        return $this->render('admin/categories/ajout.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
